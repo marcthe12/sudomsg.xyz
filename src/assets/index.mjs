f54d224d37ea731624c275d1627d5781a4b66404
Marc Pervaz Boocha <mboocha@sudomsg.xyz>
2022-01-08 18:26:11 +0530

    fixed the service worker generator

--- src/assets/index.mjs
+++ src/assets/index.mjs
@@ -1,11 +1,7 @@
-const main = async () => {
-    window.addEventListener('load', async () => {
+const main = async() => {
+    window.addEventListener('load', async() => {
         if ('serviceWorker' in navigator) {
-            try {
-                var sw = await navigator.serviceWorker.register('/sw.js')
-            } catch {
-                console.log("Service Worker failed to register")
-            }
+            navigator.serviceWorker.register('/sw.js')
         }
     })
 }
